9ba5da58ce0e8035ed5ff6e4884ba04851558609
Pradumn Upadhyay <pradumn.upadhyay1@gmail.com>
2021-04-26 19:23:48 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,5 @@
 const mongoose=require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+.then(()=>{console.log("DB connected!")})
+.catch(err => console.log("DB Error: ",err))
